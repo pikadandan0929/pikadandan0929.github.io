994c691ae3fe5310184cf292f0cf9d966bf5899d
pikadandan0929 <164056072+pikadandan0929@users.noreply.github.com>
2024-05-15 15:33:26 -0500

    final

--- js/TP7test.js
+++ js/TP7test.js
@@ -1,3 +1,4 @@
+// Version using XHR
 document.addEventListener('DOMContentLoaded', () => {
   const uuid = document.getElementById('ochreContainer').getAttribute('data-uuid');
   const ochre_url = "https://ochre.lib.uchicago.edu/ochre?uuid=";
@@ -5,16 +6,20 @@ document.addEventListener('DOMContentLoaded', () => {
 
   loadXML();
 
+  // Create first function to load the XML
   function loadXML() {
     requestXML(link, {redirect: 'follow'});
     console.log('loadXML -- OK');
   }
 
+  // Create function to call from OCHRE API and retrieve results
+  // using an XHR object
   function requestXML(link) {
     var connect = new XMLHttpRequest();
     connect.onreadystatechange = function() {
       if (this.readyState == 4 && this.status == 200) {
-        parseXML(this.responseXML);
+        parseXML(this.responseXML);  // This passes the XML file we requested to our
+                                     // next function 
       }
     };
     connect.open("GET", link, true);
@@ -22,12 +27,16 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('requestXML -- OK');
   };
 
+  // Create third function to extract properties and desired images from
+  // OCHRE XML
   function parseXML(sourceXML) {
 
+    // Extract title of item for the webpage
     var textTitle = sourceXML.getElementsByTagName('identification');
     var title_string = document.createTextNode(textTitle[1].textContent);
     document.getElementById('title').appendChild(title_string);
 
+    // Extract properties of item for the wepbage
     if (sourceXML.getElementsByTagName('property').length > 1) {
       const properties = sourceXML.querySelectorAll('property');
       i = 0;
@@ -50,6 +59,7 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     }
     
+  // Extract preview image of item for the webpage
     if (sourceXML.getElementsByTagName('resource')[0].getAttribute("format") == 'image/jpeg') {
       var img = document.createElement('img');
       var src = link + "&preview";
